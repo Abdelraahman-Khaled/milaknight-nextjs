3f6b484ca1103c1f314fda061dad65b9f0cd5e01
Abdelrahman Khaled <109894754+Abdelraahman-Khaled@users.noreply.github.com>
2026-02-17 15:04:20 +0200

    fix links footer

--- app/data/footerData.js
+++ app/data/footerData.js
@@ -1,74 +1,74 @@
 export const footerData = {
-    ar: {
-        letsWork: {
-            subTitle: "دعونا نتعاون",
-            title: "دعونا نعمل معاً",
-            buttonText: "تواصل معنا"
-        },
-        quickLinks: {
-            title: "روابط سريعة",
-            links: [
-                { text: "الرئيسية", href: "./" },
-                { text: "من نحن", href: "about.html" },
-                { text: "أعمالنا", href: "projects.html" },
-                { text: "أسعارنا", href: "pricing.html" },
-                { text: "تواصل معنا", href: "contact.html" }
-            ]
-        },
-        services: {
-            title: "الخدمات",
-            links: [
-                { text: "تصميم و برمجة المواقع", href: "service/web-development.html" },
-                { text: "وسائل التواصل الاجتماعي", href: "service/digital-marketing.html" },
-                { text: "التصميم الجرافيكي", href: "service/graphic-design.html" },
-                { text: "إنتاج الفيديو", href: "service/video-production.html" },
-                { text: "التجارة الإلكتروني", href: "service/e-commerce.html" }
-            ]
-        },
-        contact: {
-            phone: "+971 58 585 6774",
-            email: "Info[at]milaknights[dot]com"
-        },
-        copyright: "جميع الحقوق محفوظة © Milaknight LLC-FZ 2025",
-        floatingButtons: {
-            meeting: "طلب إجتماع",
-            consultation: "استشارة مجانية"
-        }
+  ar: {
+    letsWork: {
+      subTitle: "دعونا نتعاون",
+      title: "دعونا نعمل معاً",
+      buttonText: "تواصل معنا",
     },
-    en: {
-        letsWork: {
-            subTitle: "Let's Collaborate",
-            title: "Let's Work Together",
-            buttonText: "Contact Us"
-        },
-        quickLinks: {
-            title: "Quick Links",
-            links: [
-                { text: "Home", href: "./" },
-                { text: "About Us", href: "about.html" },
-                { text: "Our Work", href: "projects.html" },
-                { text: "Pricing", href: "pricing.html" },
-                { text: "Contact Us", href: "contact.html" }
-            ]
-        },
-        services: {
-            title: "Services",
-            links: [
-                { text: "Web Design & Development", href: "service/web-development.html" },
-                { text: "Social Media", href: "service/digital-marketing.html" },
-                { text: "Graphic Design", href: "service/graphic-design.html" },
-                { text: "Video Production", href: "service/video-production.html" },
-                { text: "E-Commerce", href: "service/e-commerce.html" }
-            ]
-        },
-        contact: {
-            phone: "+971 58 585 6774",
-            email: "Info[at]milaknights[dot]com"
-        },
-        copyright: "All Rights Reserved © Milaknight LLC-FZ 2025",
-        floatingButtons: {
-            meeting: "Request a Meeting",
-            consultation: "Free Consultation"
-        }
-    }
+    quickLinks: {
+      title: "روابط سريعة",
+      links: [
+        { text: "الرئيسية", href: "/" },
+        { text: "من نحن", href: "about" },
+        { text: "أعمالنا", href: "projects" },
+        { text: "أسعارنا", href: "pricing" },
+        { text: "تواصل معنا", href: "contact" },
+      ],
+    },
+    services: {
+      title: "الخدمات",
+      links: [
+        { text: "تصميم و برمجة المواقع", href: "/service/web-development" },
+        { text: "وسائل التواصل الاجتماعي", href: "/service/digital-marketing" },
+        { text: "التصميم الجرافيكي", href: "/service/graphic-design" },
+        { text: "إنتاج الفيديو", href: "/service/video-production" },
+        { text: "التجارة الإلكتروني", href: "/service/e-commerce" },
+      ],
+    },
+    contact: {
+      phone: "+971 58 585 6774",
+      email: "Info[at]milaknights[dot]com",
+    },
+    copyright: "جميع الحقوق محفوظة © Milaknight LLC-FZ 2025",
+    floatingButtons: {
+      meeting: "طلب إجتماع",
+      consultation: "استشارة مجانية",
+    },
+  },
+  en: {
+    letsWork: {
+      subTitle: "Let's Collaborate",
+      title: "Let's Work Together",
+      buttonText: "Contact Us",
+    },
+    quickLinks: {
+      title: "Quick Links",
+      links: [
+        { text: "Home", href: "/" },
+        { text: "About Us", href: "about" },
+        { text: "Our Work", href: "projects" },
+        { text: "Pricing", href: "pricing" },
+        { text: "Contact Us", href: "contact" },
+      ],
+    },
+    services: {
+      title: "Services",
+      links: [
+        { text: "Web Design & Development", href: "/service/web-development" },
+        { text: "Social Media", href: "/service/digital-marketing" },
+        { text: "Graphic Design", href: "/service/graphic-design" },
+        { text: "Video Production", href: "/service/video-production" },
+        { text: "E-Commerce", href: "/service/e-commerce" },
+      ],
+    },
+    contact: {
+      phone: "+971 58 585 6774",
+      email: "Info[at]milaknights[dot]com",
+    },
+    copyright: "All Rights Reserved © Milaknight LLC-FZ 2025",
+    floatingButtons: {
+      meeting: "Request a Meeting",
+      consultation: "Free Consultation",
+    },
+  },
 };
