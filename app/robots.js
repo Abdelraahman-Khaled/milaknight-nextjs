bf4c42e822d71a96ceef9d53c56b3ff9519218a8
Abdelrahman Khaled <109894754+Abdelraahman-Khaled@users.noreply.github.com>
2026-02-25 17:01:14 +0200

    update robots

--- app/robots.js
+++ app/robots.js
@@ -7,8 +7,6 @@ export default function robots() {
       allow: "/",
       disallow: [
         "/cdn-cgi/", // Cloudflare internal scripts
-        "/api/", // Next.js API routes
-        "/admin/", // Example private dashboard route
       ],
     },
     sitemap: "https://www.mila-knight.com/sitemap.xml",
