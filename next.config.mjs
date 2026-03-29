9605d4dd6bc0d734189e4f324942fcb96be89238
Abdelrahman Khaled <109894754+Abdelraahman-Khaled@users.noreply.github.com>
2026-03-29 12:31:11 +0200

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -25,6 +25,21 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.mila-knight.com',
+          },
+        ],
+        destination: 'https://mila-knight.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
