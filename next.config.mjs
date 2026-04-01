a3597c92ca7b0d06a5d66b89f1f1e1faf7bb87f5
Abdelrahman Khaled <109894754+Abdelraahman-Khaled@users.noreply.github.com>
2026-04-01 13:17:40 +0200

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,29 @@
 const nextConfig = {
   output: "standalone",
   images: {
-    unoptimized: true,
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mila-knight.com",
+      },
+      {
+        protocol: "https",
+        hostname: "backend.mila-knight.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.mila-knight.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.logo.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+    ],
   },
   async redirects() {
     return [
